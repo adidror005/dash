f7f94eb5066f2d152aeaed04475e34a4bf77a237
adidr <adidror005@gmail.com>
2024-06-09 08:05:29 -0400

    dfa

--- app.py
+++ app.py
@@ -4,16 +4,29 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash()
+app = Dash(__name__)
 
 app.layout = [
     html.H1(children='Welcome to Trade Mamba', style={'textAlign':'center'}),
+    html.H1(children='Youtube Channel'),
+    html.H2(children='Interactive Brokers API Videos'),
     html.Iframe(
-        src="https://www.youtube.com/embed/G2wD2scvT8w",
-        style={"height": "400px", "width": "100%"}
+        src="https://www.youtube.com/embed/videoseries?list=PLCZZtBmmgxn8CFKysCkcl-B1tqRgCCNIX",
+        width="60%",
+        height="400px",
+        style={"border": "none"}
     ),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.H2(children='LLM and RAG Videos'),
+    html.Iframe(
+        src="https://www.youtube.com/embed/videoseries?list=PLCZZtBmmgxn9DjW2Ygg-xGC_z_Tc2U5A8",
+        width="60%",
+        height="400px",
+        style={"border": "none"}
+    ),
+    html.H1(children='The ultimate trading APP Comming Soon!', style={'textAlign': 'left'}),
+
+    #dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    #dcc.Graph(id='graph-content')
 ]
 
 @callback(
@@ -26,3 +39,10 @@ def update_graph(value):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+"""
+    html.Iframe(
+        src="https://www.youtube.com/embed/G2wD2scvT8w",
+        style={"height": "400px", "width": "100%"}
+    )
+"""
