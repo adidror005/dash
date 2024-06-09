6255f02bd1b29b4459c871c87b2d13575070fe4c
adidr <adidror005@gmail.com>
2024-06-09 07:31:54 -0400

    FUCK

---
+++ app.py
@@ -0,0 +1,28 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash()
+
+app.layout = [
+    html.H1(children='Welcome to Trade Mamba', style={'textAlign':'center'}),
+    html.Iframe(
+        src="https://www.youtube.com/embed/G2wD2scvT8w",
+        style={"height": "400px", "width": "100%"}
+    ),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+]
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)
